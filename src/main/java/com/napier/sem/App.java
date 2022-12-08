4b2f505c40a0341662a5945aeec85bb3a180a5f4
a5ea5em5int <ayeayemyint1073@gmail.com>
2022-12-08 18:51:23 +0630

    created ntwork bridge in featrue branch

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -12,7 +12,9 @@ public class App
     {
         // Connect to MongoDB on local system - we're using port 27000
         try {
-            MongoClient mongoClient = new MongoClient("localhost", 27000);
+            //MongoClient mongoClient = new MongoClient("localhost", 27000);
+            // Connect to MongoDB
+            MongoClient mongoClient = new MongoClient("mongo-dbserver");
             // Get a database - will create when we use it
             MongoDatabase database = mongoClient.getDatabase("mydb");
             MongoCollection<Document> collection = database.getCollection("test");
